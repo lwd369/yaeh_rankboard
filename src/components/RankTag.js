0c3ac6573d86879cf5daffc858304590a1789129
Calvin Lee <lwd369@gmail.com>
2017-05-04 17:03:36 +0800

    change css

--- src/components/RankTag.js
+++ src/components/RankTag.js
@@ -3,10 +3,10 @@ import './RankTag.css';
 export default function RankTag(props) {
   return(
     <div className="rank-tag">
-      <div className="row-flex">
+
         <span className="star-icon"></span>
         <span className="rank-info">{props.rank}</span>
-      </div>
+
     </div>
   );
 }
